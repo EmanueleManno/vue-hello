040101f5f08e93fedcb4a907a969d7118d16f1c4
EmanueleManno <mannus1994@gmail.com>
2023-06-16 15:27:16 +0200

    Push pre-tutoring

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ const { createApp } = Vue;
 const app = createApp({
     data() {
         return {
-            h1Content: 'Bau Bau Micio Micio'
+            h1Content: 'Bau Bau Micio Micio',
+            pic: 'https://www.google.it/url?sa=i&url=https%3A%2F%2Fmypetmall.net%2Fblog%2Fpost%2Fil-gatto-europeo&psig=AOvVaw0PP9Y0VnyPIbmFQyeVuXL1&ust=1687005725020000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCKDpwpnox_8CFQAAAAAdAAAAABAE'
         }
     }
 });
